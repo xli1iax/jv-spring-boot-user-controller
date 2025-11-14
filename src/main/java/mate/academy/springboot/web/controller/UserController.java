ebabd4028c9ec93fce7ec0993eb87349d440e80a
Lilia Zhuravel <liliazuravel6@gmail.com>
2025-11-14 23:35:02 +0100

    .

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -1,9 +1,12 @@
 package mate.academy.springboot.web.controller;
 
-import mate.academy.springboot.web.model.User;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import mate.academy.springboot.web.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")

1efe473b97d270c9ec7b00f1d1de3e5b0410ba1d
Lilia Zhuravel <liliazuravel6@gmail.com>
2025-11-14 23:33:04 +0100

    added methods getUsers() and createUser(), required starters to pom.xml

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -1,5 +1,20 @@
 package mate.academy.springboot.web.controller;
 
-public class UserController {
+import mate.academy.springboot.web.model.User;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping
+    public List<User> getUsers() {
+        return List.of(new User(1L, "bob@i.ua"), new User(2L, "alice@i.ua"));
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody User user) {
+        return "User created. Id: %s, email: %s".formatted(user.id(), user.email());
+    }
 }
